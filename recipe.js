6256ce12ad60ab30ba74cd9cb08767b6680310a2
dimitriwashere <dimitriwashere@gmail.com>
2017-06-06 15:53:27 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -15,8 +15,8 @@ function updateIngredient(recipe, ingredient, amount) {
   return recipe;
 }
 
-function readRecipe(recipe) {
+function readRecipe(recipe){
   for (var key in recipe) {
-  console.log(`this recipe calls for ${recipe[key]} of ${key}`)
+    console.log("this recipe calls for " + recipe[key] + " of " + key);
   }
 }
